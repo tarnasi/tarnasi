dfe3bd3d73d6bf1c127b05f0f339a970f20888e8
tarnasi <shahryar.tarnasi@gmail.com>
2023-02-07 23:11:10 +0330

    Add project files.

---
+++ Tarnasi/Pages/Resume.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarnasi.Pages
+{
+    public class ResumeModel : PageModel
+    {
+        private readonly ILogger<ResumeModel> _logger;
+
+        public ResumeModel(ILogger<ResumeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
